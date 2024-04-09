c7720b3592dc14f67d34692c6bc4f37a99dd376d
Med Ali Naaoui <chayga00@gmail.com>
2024-04-09 22:08:42 +0000

    updating crud-api

--- crud-api/main.go
+++ crud-api/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -51,9 +53,9 @@ func deleteMovie(res http.ResponseWriter, req *http.Request){
 		if item.Id == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
 			break;
-		} else{
-			fmt.Fprintf(res, "There's no such a movie with id: %v", params["id"])
 		}
+		fmt.Fprintf(res, "There's no such a movie with id: %v", params["id"])
+
 	}
 
 	json.NewEncoder(res).Encode(movies)
@@ -72,15 +74,52 @@ func getSingleMovie(res http.ResponseWriter, req *http.Request){
 		if item.Id == params["id"] {
 			
 		json.NewEncoder(res).Encode(item)
-
-		} else{
-			fmt.Fprintf(res, "There's no such a movie with id: %v", params["id"])
-		}
+		break;	
+		} 
+		fmt.Fprintf(res, "There's no such a movie with id: %v", params["id"])
 	}
 
 
 
 }
+
+
+	func createMovie(res http.ResponseWriter, req *http.Request){
+		res.Header().Set("Content-Type", "application/json")
+		var movie Movie 
+		_ = json.NewDecoder(req.Body).Decode(&movie)
+		movie.Id = strconv.Itoa(rand.Intn(1000000) + 3333) 
+		movies = append(movies, movie)
+		json.NewEncoder(res).Encode(movie)
+	}
+
+
+	func updateMovie(res http.ResponseWriter, req *http.Request){
+
+		res.Header().Set("Content-Type","application/json")
+		params := mux.Vars(req)
+	
+	
+	
+		for  index , item := range movies {
+			if item.Id == params["id"] {
+				movies = append(movies[:index], movies[index+1:]...)
+				_ = json.NewDecoder(req.Body).Decode(&movies[index])
+				movies[index].Id = strconv.Itoa(rand.Intn(1000000) + 3333) 
+				movies = append(movies, movies[index])
+				json.NewEncoder(res).Encode(movies[index])
+
+			} else{
+				fmt.Fprintf(res, "There's no such a movie with id: %v", params["id"])
+			}
+		}
+	
+		json.NewEncoder(res).Encode(movies)
+	
+	
+	}
+
+
 
 
 
@@ -91,17 +130,29 @@ func main() {
 
 	movies = append(movies, Movie{
 		Id: "412Fqsf342",
-		Year: 2015,
+		Year: 2019,
 		Title: "The Irishman",
 		Director: &Director{"421SDQ41", "Martin  Scorsese"},
-	})
+	}, Movie{
+		Id: "2az12e23RGgd2",
+		Year: 2023,
+		Title: "Oppenheimer",
+		Director: &Director{"421SDQ41", "Christopher Nolan"},
+	},
+	Movie{
+		Id: "9SAadD8Adazd3",
+		Year: 2024,
+		Title: "Dune 2",
+		Director: &Director{"421SDQ41", "Danis Chi"},
+	},
+)
 
 	
 
 	r.HandleFunc("/movies", getAllMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getSingleMovie).Methods("GET")
-	// r.HandleFunc("/movies", createMovie).Methods("POST")
-	// r.HandleFunc("/update/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/update/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/delete/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("Server running on port 8081")
